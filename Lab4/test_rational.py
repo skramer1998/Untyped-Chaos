f607cb58e018506e2e4f39d33b36e6095dc5eea6
skramer1998 <swkramer@uwm.edu>
2019-02-13 13:40:27 -0600

    kinda working setup

--- Lab4/test_rational.py
+++ Lab4/test_rational.py
@@ -6,6 +6,11 @@ class TestRational(TestCase):
     def setUp(self):
         self.negDen = Rational(0, -2)
         self.good = Rational()
+        self.badCon = Rational('hello', '34')
     
     def test_posDen(self):
         self.assertTrue(self.negDen.d < 0)
+
+    """Test Case Ideas"""
+    def test_badConstructor(self):
+        self.assertRaises(SomeException, self.badCon)
