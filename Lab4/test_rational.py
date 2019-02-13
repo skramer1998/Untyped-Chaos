09209e4f9d0a2d07cf9ea1833aa37ac58fc22039
skramer1998 <swkramer@uwm.edu>
2019-02-13 13:35:22 -0600

    kinda working setup

--- Lab4/test_rational.py
+++ Lab4/test_rational.py
@@ -1,9 +1,11 @@
 from unittest import TestCase
 from rational import Rational
 
+
 class TestRational(TestCase):
     def setUp(self):
         self.negDen = Rational(0, -2)
+        self.good = Rational()
     
     def test_posDen(self):
         self.assertTrue(self.negDen.d < 0)
