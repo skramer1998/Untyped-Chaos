414e225aaf26430b67254a11d2c823f61252107d
Nate <ndkohls@uwm.edu>
2019-02-13 13:52:07 -0600

    ayo plz added first add test

--- Lab4/test_rational.py
+++ Lab4/test_rational.py
@@ -3,10 +3,15 @@ from rational import Rational
 
 
 class TestRational(TestCase):
+
     def setUp(self):
         self.negDen = Rational(0, -2)
         self.good = Rational()
         self.badCon = Rational('hello', '34')
+        self.rat1 = Rational(1, 2)
+        self.rat2 = Rational(2, 3)
+        self.rat3 = Rational(1, 4)
+        self.rat4 = Rational(7, 4)
     
     def test_posDen(self):
         self.assertTrue(self.negDen.d > 0)
@@ -18,3 +23,5 @@ class TestRational(TestCase):
 
     def test_add(self):
         self.assertEquals(self.add(0), self)
+        self.assertNotEqual(self.add(self), self)
+        self.assertEquals(self.add(self), )
