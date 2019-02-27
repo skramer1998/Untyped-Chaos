a2e9f6e8f9404abd4cd5c2b0f09d749da6996088
skramer1998 <swkramer@uwm.edu>
2019-02-27 13:43:16 -0600

    acceptance test stuffs

--- Project/CoursesTest.py
+++ Project/CoursesTest.py
@@ -2,4 +2,8 @@ import unittest
 
 class CoursesTest(unittest.TestCase):
     def setup(self):
-        self.fsa.command("create_course Instructor InstructorPassword")
+        """
+        self.fsa.command("create_course course_name course_number course_place course_days course_time course_semester course_professor course_tas course_labs")
+        """
+
+        self.fsa.command("create_course course_name course_number course_place course_days course_time course_semester course_professor course_tas course_labs")
