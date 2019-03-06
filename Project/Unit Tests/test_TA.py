6be8beb88c24cb47d8a23e414d4830f476aeb399
skramer1998 <swkramer@uwm.edu>
2019-03-06 14:27:31 -0600

    TA tests

--- Project/Unit Tests/test_TA.py
+++ Project/Unit Tests/test_TA.py
@@ -1,5 +1,28 @@
 from unittest import TestCase
-
+from Project.Classes.TA import TA
 
 class TestTA(TestCase):
+
+    def setUp(self):
+        self.default = TA()
+        self.namedTom = TA()
+
+    """
+    Need to test editing each piece of information that TAs are allowed to edit (personal informaton)
+    """
+    def test__editSelf__(self):
+        self.assertEqual(self.default.__editSelf__(self.default, "userName=tom"), self.namedTom)
+
+    """
+    Need to test which data gets returned and build object in setUp that replicate it
+    """
+    def test__publicInfo__(self):
+        self.assertEqual(self.default.__publicInfo__(), self.default)
+
+    """
+    Need to test assignments for courses and lab sections
+    """
+    def test__viewTAAssignments__(self):
+        self.assertEqual(self.default.__viewTAAssignments__(), "361")
+
     pass
