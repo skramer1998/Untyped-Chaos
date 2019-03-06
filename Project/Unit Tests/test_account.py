4210d278fc949b97f184a7c6713ae899957b66d3
Tyler <tylerdng18@gmail.com>
2019-03-06 14:14:52 -0600

    test

--- Project/Unit Tests/test_account.py
+++ Project/Unit Tests/test_account.py
@@ -3,10 +3,10 @@ from
 
 class TestAccount(TestCase):
     def setUp(self):
-        a1 = Acc
+
 
     def test_editSelf(self):
-        self.
+        self.assertTrue()
 
     def test_publicInfo(self):
         self.assertTrue()
