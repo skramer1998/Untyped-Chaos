e7282800aca5f4d972bb766f5159a425b5f4a43c
andresmunoz <munoz24@uwm.edu>
2019-03-06 14:17:59 -0600

    admin test

--- Project/Unit Tests/test_admin.py
+++ Project/Unit Tests/test_admin.py
@@ -1,4 +1,18 @@
 from unittest import TestCase
 
 class TestAdmin(TestCase):
-    pass
+
+    def test_createCourse(self):
+        pass
+
+    def test_createAccount(self):
+        pass
+
+    def test_modifyAccount(self):
+        pass
+
+    def test_sendAllNoti(self):
+        pass
+
+    def test_seePrivateInfo(self):
+        pass
