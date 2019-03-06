bbdec362c5442437a1dcd55d343096b99ece909d
andresmunoz <munoz24@uwm.edu>
2019-03-06 14:13:44 -0600

    account tests

---
+++ Project/Classes/Admin.py
@@ -0,0 +1,29 @@
+class Admin:
+
+    def __init__(self):
+        """Constructor for Admin."""
+        self.userName = ""
+        self.ID = ""
+        self.email = ""
+        self.phone = ""
+        self.address = ""
+
+    def __createCourse__(self, course):
+        # return data
+        pass
+
+    def __createAccount__(self, account):
+        # return data
+        pass
+
+    def __modifyAccounts__(self, account):
+        # return data
+        pass
+
+    def __sendAllNoti__(self, other):
+        # return data
+        pass
+
+    def __seePrivateInfo__(self, other):
+        # return data
+        pass
