54e639dc90402d3b9ffbd6fbef9ec4b1ad3937ff
pmmos <pjmoss@uwm.edu>
2019-03-27 13:50:51 -0500

    Message

--- Project/Acceptance Tests/CoursesTest.py
+++ Project/Acceptance Tests/CoursesTest.py
@@ -7,11 +7,11 @@ class CoursesTest(TestCase):
 
         Courses.object.create(name="softwareEngineering", number=361, place="lubarHall", days="T-TR", time="10-10:50",
                              semester="SP19", professor="rock", ta="apoorv", labs=1)
-        Course.object.create(name="algorithmDesign", number=535, place="EMS", days="M-W", time="4-5:15",
+        Courses.object.create(name="algorithmDesign", number=535, place="EMS", days="M-W", time="4-5:15",
                              semester="SP19", professor="cheng", ta=null, labs=0)
-        Course.object.create(name="security", number=469, place="EMS", days="T-TR", time="10-10:50",
+        Courses.object.create(name="security", number=469, place="EMS", days="T-TR", time="10-10:50",
                              semester="SP19", professor="bob", ta=null, labs=0)
-        Course.object.create(name="operatingSystems", number=550, place="physics", days="M-W-F", time="8-8:50",
+        Courses.object.create(name="operatingSystems", number=550, place="physics", days="M-W-F", time="8-8:50",
                              semester="SP19", professor="sorensen", ta="jimbo-allen", labs=2)
 
         #self.fsa.command("create_course softwareEngineering 361 lubarHall T-TR 10-10:50 SP19 rock apoorv 1")
