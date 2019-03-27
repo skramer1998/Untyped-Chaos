adf26d7c233233689df140384b92e45ff3ad480f
Tyler <tylerdng18@gmail.com>
2019-03-27 13:43:08 -0500

    test

--- untitled1/AppName/models.py
+++ untitled1/AppName/models.py
@@ -6,3 +6,10 @@ class MyModel(models.Model):
     fieldOne = models.CharField(max_length=20)
     fieldTwo = models.IntegerField(default=0)
     color = models.CharField(max_length=7)
+
+class AccountModel(models.Model):
+    Role = models.CharField(max_length=12)
+    Name = models.CharField(max_length=30)
+    Email = models.EmailField(max_length=30)
+    Phone = models.CharField(max_length=11)
+    Address = models.CharField(max_length=30)
