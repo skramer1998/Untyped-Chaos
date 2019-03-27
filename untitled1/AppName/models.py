3cd55704c9d39e9ea3e1125d58be1df34e8718d0
skramer1998 <swkramer@uwm.edu>
2019-03-27 13:48:30 -0500

    Account

--- untitled1/AppName/models.py
+++ untitled1/AppName/models.py
@@ -2,10 +2,13 @@
 from django.db import models
 
 # Create your models here.
+
+
 class MyModel(models.Model):
     fieldOne = models.CharField(max_length=20)
     fieldTwo = models.IntegerField(default=0)
     color = models.CharField(max_length=7)
+
 
 class AccountModel(models.Model):
     role = models.CharField(max_length=12)
@@ -13,6 +16,7 @@ class AccountModel(models.Model):
     email = models.EmailField(max_length=30)
     phone = models.CharField(max_length=11)
     address = models.CharField(max_length=30)
+
 
 class Login(models.Model):
     userName = models.CharField(max_length=30)
