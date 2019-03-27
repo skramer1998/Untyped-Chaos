6cc1c1959444a67e17c20502b68f7afe475e2f33
skramer1998 <swkramer@uwm.edu>
2019-03-27 13:49:33 -0500

    Stuff

--- Project/Acceptance Tests/CoursesTest.py
+++ Project/Acceptance Tests/CoursesTest.py
@@ -9,10 +9,10 @@ class CoursesTest(TestCase):
                              semester="SP19", professor="rock", ta="apoorv", labs=1)
 
 
-        self.fsa.command("create_course softwareEngineering 361 lubarHall T-TR 10-10:50 SP19 rock apoorv 1")
-        self.fsa.command("create_course algorithmDesign 535 EMS M-W 4-5:15 SP19 cheng")
-        self.fsa.command("create_course security 469 EMS T-TR 10-10:50 SP19 bob")
-        self.fsa.command("create_course operatingSystems 550 physics M-W-F 8-8:50 SP19 sorensen jimbo-allen 2")
+        #self.fsa.command("create_course softwareEngineering 361 lubarHall T-TR 10-10:50 SP19 rock apoorv 1")
+        #self.fsa.command("create_course algorithmDesign 535 EMS M-W 4-5:15 SP19 cheng")
+        #self.fsa.command("create_course security 469 EMS T-TR 10-10:50 SP19 bob")
+        #self.fsa.command("create_course operatingSystems 550 physics M-W-F 8-8:50 SP19 sorensen jimbo-allen 2")
 
         """
             self.fsa.command("create_course course_name course_number course_place course_days course_time course_semester course_professor course_tas course_labs")
