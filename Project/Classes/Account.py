943886cfe848072d7e81df5acfdfa369a7ef7347
Nate <ndkohls@uwm.edu>
2019-04-02 10:40:37 -0500

    Doing work to get login done: pt 1 of who knows

--- Project/Classes/Account.py
+++ Project/Classes/Account.py
@@ -8,12 +8,16 @@ class Account:
         self.userPhone = phone
         self.userAddress = address
         self.user = User.objects.create_user(self.userID, self.userEmail)
+        self.user.first_name = first
+        self.user.last_name = last
         # create an account in the user DB. No password at first default permissions for user are none individual
-        # group status should be set with the flags "is_TA" in a later step.
+        # group status should be set in a later step.
 
         # should return false if not set otherwise true
 
     def editSelf(self, name, id, email, phone, address):
+        self.user.username = name
+        self.user.userEmail = email
         self.userName = name
         self.userID = id
         self.userEmail = email
