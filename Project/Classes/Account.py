3b9c3239f12a52d0ca75995288111836909739a6
Nate <ndkohls@uwm.edu>
2019-04-03 14:49:15 -0500

    Doing work to get login done: pt 1 of who knows

--- Project/Classes/Account.py
+++ Project/Classes/Account.py
@@ -36,7 +36,6 @@ class Account:
         else:
             print("the new passwords don't match, start again from the begining.")
 
-
     def editSelf(self, name, id, email, phone, address):
         self.user.username = name
         self.user.userEmail = email
@@ -48,6 +47,88 @@ class Account:
         self.user.save()
         # should return false if not set otherwise true
 
-    def publicInfo(self, name):
+    def is_TA(self, user):
+        return user.user.groups.filter(name='TA').exists()
+
+    def is_Instructor(self, user):
+        return user.user.groups.filter(name='Instructor').exists()
+
+    def is_Admin(self, user):
+        return user.user.groups.filter(name='Admin').exists()
+
+    def is_Supervisor(self, user):
+        return user.user.groups.filter(name='Supervisor').exists()
+
+    def grantGroupStatus(self, user, groupName):  # supervisors and admins can grant permissions
+        if Account.is_Supervisor(self):
+            if groupName == "TA" or groupName == "Instructor" or groupName == "Admin" or groupName == "Supervisor":
+                user.user.group.add(groupName)
+                user.user.save()
+                print(user.userName + " has been added to "+ groupName)
+                return True
+            else:
+                print("that's not a group. the 4 groups are TA, Instructor, Admin, and Supervisor")
+                return False
+        elif Account.is_Admin(self):
+            if groupName == "TA" or groupName == "Instructor" or groupName == "Admin":
+                user.user.group.add(groupName)
+                user.user.save()
+                print(user.userName + " has been added to "+ groupName)
+                return True
+            elif groupName == "Supervisor":
+                print("Admin accounts cannot add Supervisor status")
+                return False
+            else:
+                print("that's not a group. the 4 groups are TA, Instructor, Admin, and Supervisor")
+                return False
+        else:
+            print("sign in as an Admin or Supervisor to grant group assignments")
+
+    def invalidatePassword(self, user):
+        if Account.is_Admin(self) or Account.is_Supervisor(self):
+            user.user.setUnusablePassword()
+            user.user.save()
+            return True
+        else:
+            print("only Admins and Supervisors can invalidate passwords")
+            return False
+
+    def editOther(self, user, name, id, email, phone, address):
+        if Account.is_Admin(self) or Account.is_Supervisor(self):
+            user.user.username = name
+            user.user.userEmail = email
+            user.userName = name
+            user.userID = id
+            user.userEmail = email
+            user.userPhone = phone
+            user.userAddress = address
+            user.user.save()
+            print("changes made")
+            return True
+        else:
+            print("only Admins and Supervisors can edit the account details of others")
+            return False
+        # should return false if not set otherwise true
+
+    def createCourse(self, otherargs):
+        if Account.is_Admin(self) or Account.is_Supervisor(self):
+            # call course constructor
+            return True
+        else:
+            print("Only Admins and Supervisors can add new Courses")
+            return False
+
+    def modifyCouse(self, course, otherargs):
+        return False
+        # pass shit to course edit method, if self has the permissions to do so.
+
+    def publicInfo(self, user):
         return 0
+        # big old string with whatever is actually public
+        # can include a permissions check ala other methods in this class
+
+    def toString(self):
+        return "User ID: "+self.userID+"\nUsername: "+self.user.username+"\nUserEmail: " + \
+               self.user.userEmail+"\nUserPhone: "+self.userPhone+"\nUser Address: "+self.userAddress
+
 
--- Project/Classes/Admin.py
+++ Project/Classes/Admin.py
@@ -20,6 +20,20 @@ class Admin:
         # return data
         pass
 
+    def grantGroupStatus(user, groupName):
+        if groupName == "TA" or groupName == "Instructor" or groupName == "Admin":
+            user.user.group.add(groupName)
+            user.user.save()
+            print(user.userName + " has been added to "+ groupName)
+            return True
+        elif groupName == "Supervisor":
+            print("admin accounts cannot add Supervisor status")
+            return False
+        else:
+            print("that's not a group. the 4 groups are TA, Instructor, Admin, and Supervisor")
+            return False
+
+
     def __sendAllNoti__(self, other):
         # return data
         pass
