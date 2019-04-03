21f751b56664f119f4d848c960f17e9b6c85517b
Nate <ndkohls@uwm.edu>
2019-04-03 14:51:58 -0500

    Doing work to get login done: pt 1 of who knows

---
+++ Project/Classes/Terminal.py
@@ -0,0 +1,10 @@
+# this should be a decent driver -- shout out to the whiteboard in lab
+
+class Terminal:
+    user = None
+
+    def command(str):
+        parseCommand(str)
+
+    def parseCommand(str):
+
